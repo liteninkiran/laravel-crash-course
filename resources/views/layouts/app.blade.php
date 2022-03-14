27e53beff221a55adebc7a517bf839e210f769bf
Kiran Anand <liteninkiran@hotmail.com>
2022-03-14 22:57:53 +0000

    Logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,21 +14,26 @@
 
             {{-- LHS Links --}}
             <ul class="flex items-center">
-                <li><a href="/" class="p-3">Home</a></li>
-                <li><a href="/" class="p-3">Dashboard</a></li>
-                <li><a href="/" class="p-3">Post</a></li>
+                <li><a href="{{ route('home') }}" class="p-3">Home</a></li>
+                <li><a href="{{ route('dashboard') }}" class="p-3">Dashboard</a></li>
+                <li><a href="{{ route('posts') }}" class="p-3">Post</a></li>
             </ul>
 
             {{-- RHS Links --}}
             <ul class="flex items-center">
                 @auth
                     <li><a href="/" class="p-3">Kiran Anand</a></li>
-                    <li><a href="{{ route('login') }}" class="p-3">Login</a></li>
+                    <li>
+                        <form action="{{ route('logout') }}" method="POST" class="inline p-3">
+                            @csrf
+                            <button type="submit">Logout</button>
+                        </form>
+                    </li>
                 @endauth
 
                 @guest
+                    <li><a href="{{ route('login') }}" class="p-3">Login</a></li>
                     <li><a href="{{ route('register') }}" class="p-3">Regsiter</a></li>
-                    <li><a href="/" class="p-3">Logout</a></li>
                 @endguest
             </ul>
 
